8c9ed18b5dfea65d498314ba00dac46daec5a4d4
sslgulsena7 <sisligulsena7@gmail.com>
2026-05-18 15:03:26 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
+from .ocr_module import OcrModule
+from .ner_module import NerModule
+from .sentiment_module import SentimentModule
+from .rag_module import RagModule
 
+__all__ = ["OcrModule", "NerModule", "SentimentModule", "RagModule"]
+__version__ = "1.0.0"
